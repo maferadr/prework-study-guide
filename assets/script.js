27aaeed910cacb2474da93a344afc2e5bdfd15d3
maferadr <maferadr0604@gmail.com>
2023-09-28 12:09:53 -0400

    JavaScript commands added to the project

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+var shape = "circle";
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+console.log(topics);
+
+for(var x = 0; x < topics.length; x++){
+    console.log(topics[x]);
+}
+
+var topic = "HTML";
+
+if(topic === 'HTML'){
+    console.log("Lets study HTML");
+} else if (topic === 'CSS'){
+    console.log("Lets study CSS");
+}else if(topic === 'Git'){
+    console.log("Lets study Git");
+}else if(topic === 'JavaScript'){
+    console.log("Lets study JavaScript");
+}else{
+    console.log("Please try again");
+}
+
+// var shapes = ["Triangle", "Circle", "Square", "Rectangle"]
+// console.log(shapes);
+
+// for(var x = 0; x < shapes.length; x++){
+//     console.log(shapes[x]);
+// }
